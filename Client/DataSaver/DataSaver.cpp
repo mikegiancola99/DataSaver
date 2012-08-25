bc03a6dbaa095a290825e7d13a2eb6f3e726833a
Mike Giancola <mikegiancola@gmail.com>
2012-08-24 22:32:53 -0400

    .

--- Client/DataSaver/DataSaver.cpp
+++ Client/DataSaver/DataSaver.cpp
@@ -3,7 +3,9 @@
 
 #include "stdafx.h"
 #include "resource.h"
-
+#include "TrackedDirectory.h"
+#include "DatabaseManager.h"
+#include "AppManager.h"
 
 
 class CDataSaverModule : public ATL::CAtlExeModuleT< CDataSaverModule >
@@ -15,10 +17,75 @@ CDataSaverModule _AtlModule;
 
 
 
+void InstallSystem()
+{
+    DatabaseManager dbMgr;
+    dbMgr.SetupDB();
+}
+
+void AddDir(const string& inDir)
+{
+    DatabaseManager dbMgr;
+    dbMgr.AddDir(inDir);
+}
+
+void AddType(const string& inType)
+{
+    DatabaseManager dbMgr;
+    dbMgr.AddType(inType);
+}
+
+void AddServer(const string& inServer)
+{
+    DatabaseManager dbMgr;
+    dbMgr.AddServer(inServer);
+}
+
+void ProcessCmdLine(int argc, char** argv)
+{
+    for (int i = 1; i < argc; i++)
+    {
+        string param(argv[i]);
+        std::transform(param.begin(), param.end(), param.begin(), tolower);
+        if (param.compare("install") == 0)
+            InstallSystem();
+        else if (param.compare("adddir") == 0)
+        {
+            char* tmpArg = argv[i+1];
+            if (tmpArg)
+                AddDir(tmpArg);
+        }
+        else if (param.compare("addtype") == 0)
+        {
+            char* tmpArg = argv[i+1];
+            if (tmpArg)
+                AddType(tmpArg);
+        }
+        else if (param.compare("server") == 0)
+        {
+            char* tmpArg = argv[i+1];
+            if (tmpArg)
+                AddServer(tmpArg);
+        }
+        else if (param.compare("testrun") == 0)
+        {
+            AppManager appMgr;
+            appMgr.Start();
+        }
+    }
+}
+
+
 //
 extern "C" int WINAPI _tWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, 
-								LPTSTR /*lpCmdLine*/, int nShowCmd)
+								LPTSTR lpCmdLine, int nShowCmd)
 {
+    //ProcessCmdLine(argc, argv);
+    InstallSystem();
+    AddDir("C:\\Users\\mike\\Pictures");
+    
+    AppManager appMgr;
+    appMgr.Start();
 	return _AtlModule.WinMain(nShowCmd);
 }
 
