01a521af677624d73aecb1efcec9e1a746b8bea4
Mike Giancola <mikegiancola@gmail.com>
2012-11-01 22:19:26 -0400

    initial checkin

---
+++ Client/StdAfx.h
@@ -0,0 +1,12 @@
+// ************************************************************************
+// Copyright 2012 Mike Giancola
+// All rights reserved
+// ************************************************************************
+#pragma once
+
+#include <Windows.h>
+
+#include <string>
+#include <vector>
+
+using namespace std;
